b43ad9e689bb748f5cee99d217c59427da7dd203
AlexLaMattina <alexlama@udel.edu>
2021-09-13 12:10:04 -0400

    append

--- index.js
+++ index.js
@@ -1,24 +1,10 @@
 $( document ).ready(function() {
-  var up = document.getElementsByClassName('form-group');
-  var down = document.getElementById('jumbotron');
-  up.innerHTML = "Click on the button to copy a DIV into another DIV.";
-
-    console.log( "ready!" );
+     console.log( "ready!" );
     $('#go').click(function(e){
-      $('#nameform').hide();
-    
-      $('.form-group').clone().appendTo('#jumbotron');
-      down.innerHTML = "Inner DIV is copied to another element.";
-
+      $('#nameform').submit();  
+      $(".form-group").appendTo("#jumbotron");
       e.preventDefault();
     });
 });
-//    $('button[name="delete_spec"]').click(function (e) {
-//  e.preventDefault();
-//  delete_specs($(this).data("id")); 
-//});
-//("#prospects_form").submit(function(e) {
-//    e.preventDefault();
-//})
-//;
+
 
