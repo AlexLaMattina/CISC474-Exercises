116c71f77d877556c5d75783722ba3aa3c603f8e
AlexLaMattina <alexlama@udel.edu>
2021-09-13 11:29:13 -0400

    changes

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 $( document ).ready(function() {
     console.log( "ready!" );
-    $('button[name="delete_spec"]').click(function (e) {
-        e.preventDefault();
-        delete_specs($(this).data("id")); 
-      });
+
 });
+//    $('button[name="delete_spec"]').click(function (e) {
+//  e.preventDefault();
+//  delete_specs($(this).data("id")); 
+//});
 //("#prospects_form").submit(function(e) {
 //    e.preventDefault();
 //})
